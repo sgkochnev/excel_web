7de451dda61c1b045f729328285686b250a4a257
Kraven33 <skochnev69@gmail.com>
2023-01-13 22:30:58 +0300

    added resizing of the table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,29 @@
 import ExcelComponent from '../../core/ExcelComponent'
+import shouldResize from './table.helpers'
 import createTable from './table.template'
+import resize from './table.resize'
 
 export default class Table extends ExcelComponent {
   static className = 'excel__table'
+
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown'],
+    })
+  }
+
   toHTML() {
     return createTable(100, 26)
+  }
+
+  onClick() {
+    console.log('click');
+  }
+
+  onMousedown(event) {
+    if (shouldResize(event)) {
+      resize(event, this.$root)
+    }
   }
 }
